b6d38adb8f32feefe306a2ae5e93953c776e0587
Sami Laine <sami.v.laine@gmail.com>
2021-09-06 22:56:11 +0200

    Lint shell script

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-DIR="$(cd "$(dirname $0)" && pwd)"
-name=$(basename $0)
+set -euo pipefail
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+name=$(basename "$0")
 script_name=${name%.sh}.py
-cd $DIR
-$DIR/venv/bin/python $script_name $@
+cd "$DIR" || exit 1
+"$DIR/venv/bin/python" "$script_name" "$@"
