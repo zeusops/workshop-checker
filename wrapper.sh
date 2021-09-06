2901e595e227c3c2f03125e953d18221b275a56c
Sami Laine <sami.v.laine@gmail.com>
2021-09-06 22:57:44 +0200

    exit code

--- wrapper.sh
+++ wrapper.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 DIR="$(cd "$(dirname "$0")" && pwd)"
 name=$(basename "$0")
 script_name=${name%.sh}.py
-cd "$DIR" || exit 1
+cd "$DIR" || exit
 "$DIR/venv/bin/python" "$script_name" "$@"
